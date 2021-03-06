48207c011e075b4c3a86766986028638353414df
Monali <khandelwal.monali@gmail.com>
2021-03-06 11:34:15 -0500

    Turning in PersonClass Lab

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,32 +4,41 @@ package com.zipcodewilmington.person;
  * Created by leon on 2/12/18.
  */
 public class Person {
+    // instance variable
     private String name;
     private int age;
 
     public Person() {
+        this.name="";
+        this.age=Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age; // setting instance variable
     }
 
     public Person(String name) {
+        this.name = name;      // setting instance variable
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
 }
